84b5d1d6fe5c98d6ce8509b64a73466657289bc6
Christophe Gravier <christophe.gravier@telecom-st-etienne.fr>
2013-05-08 14:02:10 +0200

    Supports the new WTA metric in menus

--- src/main/java/ciir/umass/edu/metric/MetricScorerFactory.java
+++ src/main/java/ciir/umass/edu/metric/MetricScorerFactory.java
@@ -16,11 +16,10 @@ import java.util.Hashtable;
  */
 public class MetricScorerFactory {
 
-	private static MetricScorer[] mFactory = new MetricScorer[]{new APScorer(), new NDCGScorer(), new DCGScorer(), new PrecisionScorer(), new ReciprocalRankScorer(), new BestAtKScorer(), new ERRScorer()};
+	private static MetricScorer[] mFactory = new MetricScorer[] { new APScorer(), new NDCGScorer(), new DCGScorer(), new PrecisionScorer(), new ReciprocalRankScorer(), new BestAtKScorer(), new ERRScorer() };
 	private static Hashtable<String, MetricScorer> map = new Hashtable<String, MetricScorer>();
-	
-	public MetricScorerFactory()
-	{
+
+	public MetricScorerFactory() {
 		map.put("MAP", new APScorer());
 		map.put("NDCG", new NDCGScorer());
 		map.put("DCG", new DCGScorer());
@@ -28,30 +27,30 @@ public class MetricScorerFactory {
 		map.put("RR", new ReciprocalRankScorer());
 		map.put("BEST", new BestAtKScorer());
 		map.put("ERR", new ERRScorer());
+		map.put("WTA", new WinnerTakesAllScorer());
 	}
-	public MetricScorer createScorer(METRIC metric)
-	{
+
+	public MetricScorer createScorer(METRIC metric) {
 		return mFactory[metric.ordinal() - METRIC.MAP.ordinal()].clone();
 	}
-	public MetricScorer createScorer(METRIC metric, int k)
-	{
+
+	public MetricScorer createScorer(METRIC metric, int k) {
 		MetricScorer s = mFactory[metric.ordinal() - METRIC.MAP.ordinal()].clone();
 		s.setK(k);
 		return s;
 	}
-	public MetricScorer createScorer(String metric)//e.g.: metric = "NDCG@5"
+
+	public MetricScorer createScorer(String metric)// e.g.: metric = "NDCG@5"
 	{
 		int k = -1;
 		String m = "";
 		MetricScorer s = null;
-		if(metric.indexOf("@") != -1)
-		{
+		if (metric.indexOf("@") != -1) {
 			m = metric.substring(0, metric.indexOf("@"));
-			k = Integer.parseInt(metric.substring(metric.indexOf("@")+1));
+			k = Integer.parseInt(metric.substring(metric.indexOf("@") + 1));
 			s = map.get(m.toUpperCase()).clone();
 			s.setK(k);
-		}
-		else
+		} else
 			s = map.get(metric.toUpperCase()).clone();
 		return s;
 	}
